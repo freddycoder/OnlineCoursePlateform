e5a5be2972d594bec1e062184fe7acceda2f3be2
Frédéric Jacques <gary.bob88@gmail.com>
2025-10-31 08:45:06 -0400

    improve course page and start deploying to gh page

--- Models/Models.cs
+++ Models/Models.cs
@@ -3,4 +3,18 @@ namespace OnlineLearningPlatform.Models;
 public record Course(string Title, string Description, List<Module> Modules);
 public record Module(string Title, string Description, double EstimatedHours, List<Lesson> Lessons);
 public record Lesson(string Title, string Summary, int EstimatedMinutes, List<string> LearningObjectives, string Content, List<Exercise> Exercises);
-public record Exercise(string Instruction, string Answer, string Explanation);
+
+public class Exercise
+{
+    public string Instruction { get; init; }
+    public string Answer { get; init; }
+    public string Explanation { get; init; }
+    public bool ShowAnswer { get; set; } = false;
+
+    public Exercise(string instruction, string answer, string explanation)
+    {
+        Instruction = instruction;
+        Answer = answer;
+        Explanation = explanation;
+    }
+}
