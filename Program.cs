61cbf8acf29b320b171518470f72ae6064a1a670
Frédéric Jacques <gary.bob88@gmail.com>
2025-11-03 20:17:17 -0500

    work in progress, converting the app to a webassembly app

--- Program.cs
+++ Program.cs
@@ -1,29 +1,13 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OnlineCoursePlateform.Components;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.RootComponents.Add<App>("#app");
 builder.Services.AddSpeechSynthesis();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
 
 await app.RunAsync();
